a36b17e57f1f34f2a62bdae1661d0630a740d644
Naarii <naarii94@gmail.com>
2022-10-11 10:38:16 +0100

    Home page set up

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,74 @@
 import React from "react";
-import homeBanner from "../images/HomeBanner/HomeBanner.png";
+import HomeBanner from "../images/HomeBanner/HomeBanner.png";
+import PlaceholderImg from "../images/placeholder.jpg";
+import FortropolisImg from "../images/films/fortropolis/nowshowing/fortropolis.png";
+import ThePoolImg from "../images/films/thepool/nowshowing/thepool.jpg";
+import FrancescaImg from "../images/films/francesca/nowshowing/francesca.jpg";
+import PostcardsImg from "../images/films/postcardsfromparis/nowshowing/postcardsfromparis.jpg";
+import SwampImg from "../images/films/welcometotheswamp/nowshowing/welcometotheswamp.png";
+import FleetingImg from "../images/films/fleetingbloom/nowshowing/fleetingbloom.jpg";
 
 const Home = (props) => {
     return (<div>
-                <img src={homeBanner}/>       
+                <img className="top" src={HomeBanner}/>       
+                <div className="left">
+                    <p>
+                    Donec fringilla orci sagittis tempor tincidunt. Sed nec luctus erat. Nunc posuere aliquet sem, eget feugiat arcu. 
+                    Vivamus vel luctus orci. Vivamus quis sem sapien. Aliquam aliquet dolor vel cursus feugiat. Integer nunc ipsum, 
+                    pellentesque eget consectetur ac, vestibulum et mauris. Morbi feugiat enim eget sem rutrum congue. Donec eleifend 
+                    sodales dictum.
+                    </p>
+                    <img className="middle" src={PlaceholderImg} />
+                    <p>
+                    Ut faucibus eros sit amet eros hendrerit, at luctus risus maximus. Proin porttitor consectetur diam, id aliquet eros ornare non. 
+                    Mauris tempor fringilla diam quis ultricies. Nulla facilisi. Sed egestas volutpat egestas. Cras ornare risus ut eros malesuada, 
+                    at egestas libero ornare. Nulla non lacus dapibus, ullamcorper augue vel, ultrices nibh. Aliquam vulputate mi sit amet faucibus euismod. 
+                    Fusce congue scelerisque lectus, non mattis nulla mattis a.
+                    </p>
+
+                </div>
+                <div className="right">
+                <p>
+                    Donec fringilla orci sagittis tempor tincidunt. Sed nec luctus erat. Nunc posuere aliquet sem, eget feugiat arcu. 
+                    Vivamus vel luctus orci. Vivamus quis sem sapien. Aliquam aliquet dolor vel cursus feugiat. Integer nunc ipsum, 
+                    pellentesque eget consectetur ac, vestibulum et mauris. Morbi feugiat enim eget sem rutrum congue. Donec eleifend 
+                    sodales dictum.
+                    </p>
+                    <img className="bottom" src={PlaceholderImg} />
+                </div>
+                <div>
+                    <h4>NOW SHOWING</h4>
+                    <table>
+                        <tr>
+                            <td className="now-showing-card">
+                                <img src={FortropolisImg}/>
+                                <h4>FORTROPOLIS</h4>
+                            </td>
+                            <td className="now-showing-card">
+                                <img src={ThePoolImg}/>
+                                <h4>THE POOL</h4>
+                            </td>
+                            <td className="now-showing-card">
+                                <img src={FrancescaImg}/>
+                                <h4>FRANCESCA</h4>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="now-showing-card">
+                                <img src={PostcardsImg}/>
+                                <h4>POSTCARDS FROM PARIS</h4>
+                            </td>
+                            <td className="now-showing-card">
+                                <img src={SwampImg}/>
+                                <h4>WELCOME TO THE SWAMP</h4>
+                            </td>
+                            <td className="now-showing-card">
+                                <img src={FleetingImg}/>
+                                <h4>FLEETING BLOOM</h4>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
     </div>);
 }
 
